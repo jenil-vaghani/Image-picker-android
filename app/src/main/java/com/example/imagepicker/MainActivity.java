f1285a6cf3bfdfca1432565f61d82c472db9f02f
jenil-vaghani <jenilvaghani001@gmail.com>
2021-02-03 22:36:24 +0530

    Image Picker Android Using Java Language

---
+++ app/src/main/java/com/example/imagepicker/MainActivity.java
@@ -0,0 +1,49 @@
+package com.example.imagepicker;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button btn;
+    ImageView imageView;
+    int SELECT_IMAGE_CODE = 1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btn = findViewById(R.id.btn);
+        imageView = findViewById(R.id.pickedImage);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent();
+                intent.setType("image/*");
+                intent.setAction(Intent.ACTION_GET_CONTENT);
+                startActivityForResult(Intent.createChooser(intent, "Title"), SELECT_IMAGE_CODE);
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == 1) {
+            Uri uri=data.getData();
+            imageView.setImageURI(uri);
+            btn.setText("Done");
+        }
+
+    }
+}
